94081de82a237589bbf55e4ee13458981edef356
Joel Rinos <jrinos@ad.acquireap>
2018-03-26 21:49:04 -0700

    test

--- namespace_demo/demo2.php
+++ namespace_demo/demo2.php
@@ -4,21 +4,25 @@
 <?php
 
 
-include "fns.php";
+include "fns.php"; // include_once
+require "sns.php"; // require_once
 
-include "sns.php";
 
+use second\A;
 
 /* qualified class name */
-$object1 = new second\A;
+//$object1 = new second\A;
+$object = new A; 
+
 
 echo "<br>";
+
 
 // unqualified class name;
 //$sObjectA = new A;
 
-/* fully qualified class name FQCN */
-$sObjectB = new \A;
+/* FQCN: fully qualified class name */
+$object = new \A;
 
 
 ?>
